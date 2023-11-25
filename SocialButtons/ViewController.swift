26ff65feded6618dbe6a1e427e10fad1959f7a4e
OITD <oitd@banxico.org.mx>
2023-11-24 18:36:26 -0600

    Initial Commit

---
+++ SocialButtons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialButtons
+//
+//  Created by OITD on 24/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
